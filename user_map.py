01bcb9518c3eca3f0ac99f6edb8b077abf181b8c
YourFitnessSensei <vdowin.pawel@gmail.com>
2025-05-22 14:08:22 +0300

    Update user_map.py

--- user_map.py
+++ user_map.py
@@ -1,1 +1,14 @@
-user_map = {}
+import json
+import os
+
+USER_MAP_FILE = "user_map.json"
+
+try:
+    with open(USER_MAP_FILE, "r") as f:
+        user_map = json.load(f)
+except (FileNotFoundError, json.JSONDecodeError):
+    user_map = {}
+
+def save_user_map():
+    with open(USER_MAP_FILE, "w") as f:
+        json.dump(user_map, f)
